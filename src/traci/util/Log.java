048fe8a33cd5cfccce467717af71e7f22aa92403
Erik Pettersson <erik@ejp.se>
2013-01-19 17:17:32 +0000

    Consistent output messages.

--- src/traci/util/Log.java
+++ src/traci/util/Log.java
@@ -1,56 +1,71 @@
 package traci.util;
 
+import java.io.PrintStream;
+
 public class Log
 {
     public enum LogLevel
     {
-        ERROR,
-        WARNING,
-        INFO,
-        DEBUG
+        ERROR  ("[ERROR]   ", "          ", System.err),
+        WARNING("[WARNING] ", "          ", System.err),
+        INFO   ("[INFO]    ", "          ", System.out),
+        DEBUG  ("[DEBUG]   ", "          ", System.out);
+
+        private final String prefixOne;
+        private final String prefixTwo;
+        private final PrintStream printStream;
+
+        private LogLevel(final String prefixOne, final String prefixTwo, final PrintStream printStream)
+        {
+            this.prefixOne = prefixOne;
+            this.prefixTwo = prefixTwo;
+            this.printStream = printStream;
+
+        }
     }
+
     private static LogLevel logLevel = LogLevel.INFO;
 
-    public static void ERROR(final String msg)
+    private static void handleMessage(final LogLevel level, final String msg)
     {
-        if (logLevel.compareTo(LogLevel.ERROR) >= 0)
+        if (logLevel.compareTo(level) >= 0)
         {
             final String[] lines = msg.split("\n");
+
             for (int i = 0; i < lines.length; ++i)
             {
+                final String line;
                 if (i == 0)
                 {
-                    System.err.println("[ERROR] " + lines[i]);
+                    line = level.prefixOne + lines[i];
                 }
                 else
                 {
-                    System.err.println("        " + lines[i]);
+                    line = level.prefixTwo + lines[i];
                 }
+
+                level.printStream.println(line);
             }
         }
     }
 
+    public static void ERROR(final String msg)
+    {
+        handleMessage(LogLevel.ERROR, msg);
+    }
+
     public static void WARNING(final String msg)
     {
-        if (logLevel.compareTo(LogLevel.WARNING) >= 0)
-        {
-            System.out.println("[WARNING] " + msg);
-        }
+        handleMessage(LogLevel.WARNING, msg);
     }
 
     public static void INFO(final String msg)
     {
-        if (logLevel.compareTo(LogLevel.INFO) >= 0)
-        {
-            System.out.println("[INFO] " + msg);
-        }
+        handleMessage(LogLevel.INFO, msg);
     }
 
     public static void DEBUG(final String msg)
     {
-        if (logLevel.compareTo(LogLevel.DEBUG) >= 0)
-        {
-            System.out.println("[DEBUG] " + msg);
-        }
+        handleMessage(LogLevel.DEBUG, msg);
     }
 }
