93da8ad26852cc6fcee3018847bd6df36eb81515
Erik Pettersson <erik@ejp.se>
2013-05-27 19:41:33 +0000

    Test image.

--- test/se/ejp/traci/lang/interpreter/node/InterpreterPigmentTest.java
+++ test/se/ejp/traci/lang/interpreter/node/InterpreterPigmentTest.java
@@ -6,12 +6,15 @@ import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 
 import se.ejp.traci.lang.interpreter.TraciValue.Type;
+import se.ejp.traci.lang.interpreter.exceptions.InterpreterIOException;
 import se.ejp.traci.lang.interpreter.exceptions.InterpreterRuntimeException;
+import se.ejp.traci.math.Projection2D;
 import se.ejp.traci.math.Transformations;
 import se.ejp.traci.math.Vector;
 import se.ejp.traci.model.Color;
 import se.ejp.traci.model.material.pigment.Checker;
 import se.ejp.traci.model.material.pigment.FileImage;
+import se.ejp.traci.model.material.pigment.RepeatPolicy;
 import se.ejp.traci.model.material.pigment.Solid;
 
 public class InterpreterPigmentTest extends InterpreterObjectBase
@@ -62,10 +65,30 @@ public class InterpreterPigmentTest extends InterpreterObjectBase
     @Test
     public void testImage() throws RecognitionException, InterpreterRuntimeException
     {
-        final String args = "\"images/lenna.png\", \"repeat\", \"cylinder\"";
+        final String args1 = "\"images/lenna.png\", \"repeat\", \"cylinder\"";
+        final String args2 = "\"images/lenna.png\", \"border\", \"xy\", color [1,1,1]";
         final String modifiers = "rotx 2.23;";
 
-        runTests("image", FileImage.class, args, null);
-        runTests("image", FileImage.class, args, modifiers);
+        for (final FileImage fileImage : runTests("image", FileImage.class, args1, null))
+        {
+            assertEquals("images/lenna.png", fileImage.filename);
+            assertEquals(RepeatPolicy.REPEAT, fileImage.repeatPolicy);
+            assertEquals(Projection2D.CYLINDER, fileImage.projection);
+            assertEquals(Color.BLACK, fileImage.borderColor);
+            assertEquals(Transformations.identity(), fileImage.getTransformation());
+        }
+
+        for (final FileImage fileImage : runTests("image", FileImage.class, args2, modifiers))
+        {
+            assertEquals("images/lenna.png", fileImage.filename);
+            assertEquals(RepeatPolicy.BORDER, fileImage.repeatPolicy);
+            assertEquals(Projection2D.XY_PLANE, fileImage.projection);
+            assertEquals(Color.WHITE, fileImage.borderColor);
+            assertEquals(Transformations.rotx(2.23), fileImage.getTransformation());
+        }
+
+        runTestsFail(InterpreterIOException.class, "image", "\"non/existing/file.png\", \"repeat\", \"cylinder\"", null);
+        runTestsFail(InterpreterRuntimeException.class, "image", "\"images/lenna.png\", \"knas\", \"cylinder\"", null);
+        runTestsFail(InterpreterRuntimeException.class, "image", "\"images/lenna.png\", \"repeat\", \"knas\"", null);
     }
 }
